3e0b904d8b6908c2b28a776a877fd274c88973c9
koreops <ajatt5@gmail.com>
2015-09-29 17:11:28 -0400

    eggs

--- h2o/scipy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Install package SciPy
-wget -nc http://archive.ubuntu.com/ubuntu/pool/universe/p/python-scipy/python-scipy_0.9.0+dfsg1.orig.tar.gz
-tar -zxf python-scipy_0.9.0+dfsg1.orig.tar.gz
